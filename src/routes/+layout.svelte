b27ee7c0ef8fbcc4ecb9817b53de1f4762c42a04
iamgabrieltv <gabrielsarcevic1@gmail.com>
2025-12-27 04:39:35 +0100

    yes part 2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<main class="p-4">
+	<h1 class="text-center text-7xl md:text-9xl">IamFaucet</h1>
+
+	{@render children()}
+</main>
